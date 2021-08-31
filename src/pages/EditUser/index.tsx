17c3cec2caf20b59538ee65fd5438166ce1ada0c
alailsonko <aalailson3@gmail.com>
2021-08-31 00:00:20 -0300

    refactoring edituser not completed

--- src/pages/EditUser/index.tsx
+++ src/pages/EditUser/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React, { useState } from 'react';
 import './styles.css';
 import ArrowBack from '@material-ui/icons/ArrowBackIos';
@@ -5,7 +6,10 @@ import { makeStyles, alpha, withStyles } from '@material-ui/core/styles';
 import InputBase from '@material-ui/core/InputBase';
 import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForwardIos';
+import CloseIcon from '@material-ui/icons/Close';
 import Logo from '../../components/Logo';
+import InputField from '../../components/InputField';
 import UserName from '../../components/UserName';
 import Selfie from '../../assets/Selfie@3x.png';
 import useGlobalStyles from '../../styles/defaultStyles';
@@ -17,16 +21,16 @@ const BootstrapInput = withStyles((theme) => ({
     alignItems: 'center',
     borderRadius: 0,
     position: 'relative',
-    width: theme.spacing(30),
+    width: theme.spacing(24),
     height: theme.spacing(5),
     paddingLeft: '18px',
     fontSize: '18pt',
     fontFamily: ['GilroyMedium'].join(','),
-    '&:focus': {
-      color: '#14173D',
-      fontSize: '18pt',
-      border: '2px solid #1FD6BA',
-    },
+    // '&:focus': {
+    //   color: '#14173D',
+    //   fontSize: '18pt',
+    //   border: '0.1px solid #1FD6BA',
+    // },
     '&::placeholder': {
       color: '#14173D',
       fontSize: '18pt',
@@ -51,24 +55,39 @@ const useStyles = makeStyles((theme) => ({
   editButton: {
     textTransform: 'none',
     color: '#1FD6BA',
+    minWidth: '54px',
   },
   deleteButton: {
     textTransform: 'none',
     color: '#EA573E',
+    minWidth: '54px',
+  },
+  inputWrapper: {
+    position: 'relative',
+  },
+  iconWrapper: {
+    position: 'absolute',
+    bottom: theme.spacing(1.4),
+    right: '0',
   },
 }));
 
 interface IFieldEditUser {
-  value?: string | any;
-  isEditing?: boolean;
+  value: string | any;
+  isEditing: boolean;
 }
 
 interface IEditUser {
-  name?: IFieldEditUser;
+  name: IFieldEditUser;
+  username: IFieldEditUser;
 }
 
 const initialStateEditUser: IEditUser = {
   name: {
+    value: '',
+    isEditing: false,
+  },
+  username: {
     value: '',
     isEditing: false,
   },
@@ -82,6 +101,51 @@ const EditUser: React.FC = () => {
     setEditUser({
       ...editUser,
       [field]: {
+        value,
+        isEditing: true,
+      },
+    });
+  };
+  const handleSaveButton = (field: string, value: string) => {
+    setEditUser({
+      ...editUser,
+      [field]: {
+        value,
+        isEditing: false,
+      },
+    });
+  };
+  const handleCancelButton = () => {
+    console.log('cancel');
+  };
+  const testingHandleEditButton = (name: string, value: string) => {
+    console.log(name, value);
+    setEditUser({
+      ...editUser,
+      [name]: {
+        value,
+        isEditing: true,
+      },
+    });
+  };
+  const testingHandleCancelButton = (name: string) => {
+    console.log(name);
+  };
+  const testingHandleSaveButton = (name: string, value: string) => {
+    console.log(name, value);
+    setEditUser({
+      ...editUser,
+      [name]: {
+        value,
+        isEditing: false,
+      },
+    });
+  };
+  const testingHandleOnChange = (name: string, value: string) => {
+    console.log(name, value);
+    setEditUser({
+      ...editUser,
+      [name]: {
         value,
         isEditing: true,
       },
@@ -106,10 +170,13 @@ const EditUser: React.FC = () => {
         <img className={classes.selfieIcon} src={Selfie} alt="selfie" />
       </div>
       <UserName classStyle={globalClasses.h3} title="Edit your profile" />
-      <div>
-        <InputLabel shrink htmlFor="name">
-          name
-        </InputLabel>
+      <div className={classes.inputWrapper}>
+        {editUser.name && !editUser.name.isEditing ? (
+          <InputLabel shrink htmlFor="name">
+            name
+          </InputLabel>
+        ) : null}
+
         <BootstrapInput
           disabled={!!(editUser.name && !editUser.name.isEditing)}
           id="name"
@@ -135,17 +202,73 @@ const EditUser: React.FC = () => {
               ? {
                   width: '210px',
                 }
+              : editUser.name && editUser.name.isEditing
+              ? {
+                  border: '2px solid #1FD6BA',
+                  paddingRight: '108px',
+                }
               : {}
           }
         />
-        <Button className={classes.deleteButton}>Delete</Button>
-        <Button
-          onClick={() => handleEditButton('name', 'John Cena')}
-          className={classes.editButton}
-        >
-          Edit
-        </Button>
+        {editUser.name && !editUser.name.isEditing ? (
+          <>
+            <Button className={classes.deleteButton}>Delete</Button>
+            <Button
+              onClick={() => handleEditButton('name', 'John Cena')}
+              className={classes.editButton}
+            >
+              Edit
+            </Button>
+          </>
+        ) : (
+          <div className={classes.iconWrapper}>
+            <Button
+              classes={{
+                root: classes.deleteButton,
+              }}
+              className={classes.deleteButton}
+              onClick={handleCancelButton}
+            >
+              <CloseIcon />
+            </Button>
+            <Button
+              classes={{
+                root: classes.editButton,
+              }}
+              className={classes.editButton}
+              onClick={() => {
+                if (editUser.name) {
+                  handleSaveButton('name', editUser.name.value);
+                }
+              }}
+            >
+              <ArrowForwardIcon />
+            </Button>
+          </div>
+        )}
       </div>
+      <InputField
+        field={editUser.name}
+        handleCancelButton={testingHandleCancelButton}
+        handleSaveButton={testingHandleSaveButton}
+        handleEditButton={testingHandleEditButton}
+        handleOnChange={testingHandleOnChange}
+        idInput="name"
+        initialValue="John Cena"
+        labelName="name"
+        nameInput="name"
+      />
+      <InputField
+        field={editUser.username}
+        handleCancelButton={testingHandleCancelButton}
+        handleSaveButton={testingHandleSaveButton}
+        handleEditButton={testingHandleEditButton}
+        handleOnChange={testingHandleOnChange}
+        idInput="username"
+        initialValue="Invisible john"
+        labelName="Username"
+        nameInput="username"
+      />
     </div>
   );
 };
