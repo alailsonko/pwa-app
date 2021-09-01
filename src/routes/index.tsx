9f2596cf797b299ea75e04467f8730c7d077616a
alailsonko <aalailson3@gmail.com>
2021-08-31 22:25:34 -0300

    implements auth routes and private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector, connect } from 'react-redux';
 import Home from '../pages/Home';
 import Register from '../pages/Register';
 import EditUser from '../pages/EditUser';
+import PrivateRoute from './PrivateRoute';
+import AuthRoute from './AuthRoute';
 
-export default function Routes(): JSX.Element {
+function Routes(): JSX.Element {
+  const isLogged = useSelector((state: any) => state.signup.isLogged);
+  const loading = useSelector((state: any) => state.signup.loading);
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/edit-user" exact component={EditUser} />
+        <AuthRoute
+          path="/"
+          exact
+          loading={loading}
+          isLogged={isLogged}
+          component={Home}
+        />
+        <AuthRoute
+          path="/register"
+          exact
+          loading={loading}
+          isLogged={isLogged}
+          component={Register}
+        />
+        <PrivateRoute
+          path="/edit-user"
+          exact
+          component={EditUser}
+          loading={loading}
+          isLogged={isLogged}
+        />
       </Switch>
     </Router>
   );
 }
+
+export default connect()(Routes);
