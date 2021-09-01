4e87967eda39b84f8825cc82a4323a2df03103ce
alailsonko <aalailson3@gmail.com>
2021-08-31 22:24:40 -0300

    create auth route

---
+++ src/routes/AuthRoute/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+
+interface AuthRouteProps extends RouteProps {
+  component: any;
+  isLogged: boolean;
+  loading: boolean;
+}
+
+const AuthRoute: React.FC<AuthRouteProps> = (props: AuthRouteProps) => {
+  const { component: Component, isLogged, ...rest } = props;
+
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) =>
+        !isLogged ? (
+          <Component {...routeProps} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/edit-user',
+              state: { from: routeProps.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default AuthRoute;
